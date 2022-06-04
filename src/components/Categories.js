303c3d5bf44a8f25bbab24f76133ae1688666cec
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-04 23:31:32 +0300

    Add components

---
+++ src/components/Categories.js
@@ -0,0 +1,6 @@
+const catigories = () => (
+  <div>
+    <h1>it is empty now</h1>
+  </div>
+);
+export default catigories;
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/catigorise',
+      text: 'Catigorise',
+
+    },
+  ];
+  return (
+    <header>
+      <nav>
+        <div className="navbar">
+          <h1>Book Stores</h1>
+          <ul>
+            {links.map((link) => (
+              <li key={link.id}>
+
+                <Link to={link.path} className="nav_link" exact>
+                  {link.text}
+
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
