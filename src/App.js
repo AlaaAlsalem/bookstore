44571f58760f08a081363903b3f1a2f927358332
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-04 23:35:07 +0300

    Fix styleint errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Books from './components/Books';
+import Categories from './components/Categories';
+import Navbar from './components/Navbar';
+import BookInput from './components/BookInput';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Books />
+          <BookInput />
+        </Route>
+        <Route path="/catigories">
+          <Categories />
+        </Route>
+
+      </Switch>
     </div>
   );
 }
