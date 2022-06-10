b283ecc087f6ea7a6b79b690d47b3faed5f4d002
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-10 21:35:41 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
 
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
@@ -13,7 +13,7 @@ root.render(
       <App />
     </Router>
   </React.StrictMode>,
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
