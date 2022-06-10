06c4c12d9b86cabd215ef0b732e5c63a02e13da4
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-06-10 18:59:31 +0300

    Add logic to BookInput.js so the user can add bookes to state

--- src/components/BookInput.js
+++ src/components/BookInput.js
@@ -1,11 +1,60 @@
-const Bookinput = () => (
-  <section>
-    <form className="form">
-      <input type="text" placeholder="Book Title" />
-      <input type="text" placeholder="Book Author" />
-      <button type="submit">Add the Book</button>
-    </form>
-  </section>
-);
+import { React, useState } from 'react';
+import PropTypes from 'prop-types';
 
-export default Bookinput;
+const BookInput = (props) => {
+  const [input, setInput] = useState({
+    title: '',
+    author: '',
+  });
+
+  const onChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const { propsToAddBooks } = props;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.title.trim() && input.author.trim()) {
+      propsToAddBooks(input.title, input.author);
+      setInput({
+        title: '',
+        author: '',
+      });
+    }
+  };
+
+  return (
+    <section>
+      <h2>Add New Book</h2>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book Title"
+          value={input.title}
+          name="title"
+          onChange={onChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Book Author"
+          value={input.author}
+          name="author"
+          onChange={onChange}
+          required
+        />
+        <button type="submit" className="submit-btn">Add Book</button>
+      </form>
+    </section>
+  );
+};
+
+BookInput.propTypes = {
+  propsToAddBooks: PropTypes.func.isRequired,
+};
+
+export default BookInput;
